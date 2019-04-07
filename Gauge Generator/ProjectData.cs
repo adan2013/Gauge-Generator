f6e4de788fd5c669ddd18e9f01fda06bd2c42cbe
adan2013 <adan2013@poczta.fm>
2019-04-07 16:58:25 +0200

    first code

---
+++ Gauge Generator/ProjectData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauge_Generator
+{
+    [Serializable()]
+    class ProjectData
+    {
+        int pImageSize = 100;
+        List<Layer> layers = new List<Layer>();
+
+        public int ImageSize
+        {
+            get { return pImageSize; }
+            set
+            {
+                if(value >= 100 && value <= 1000)
+                {
+                    pImageSize = value;
+                    //TODO event size changed
+                }
+            }
+        }
+    }
+}
