cc3f81ee9b48ca3df5668ed6192ec6e0c2730730
adan2013 <adan2013@poczta.fm>
2019-05-09 10:23:05 +0200

    first beta version

--- Gauge Generator/Properties/AssemblyInfo.cs
+++ Gauge Generator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("Gauge Generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("adan2013")]
 [assembly: AssemblyProduct("Gauge Generator")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
